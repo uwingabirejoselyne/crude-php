d4cf5348f599c262b439ad4befe4363da7b82e3a
uwingabirejoselyne <uwingajoselyne@gmail.com>
2024-10-24 21:28:53 +0200

    client creation

---
+++ create.php
@@ -0,0 +1,110 @@
+<?php
+include 'db_connect.php';
+
+$name = '';
+$email = '';
+$phone = '';
+$address = '';
+$errorMessage = '';
+$successMessage = '';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $address = $_POST['address'];
+
+    do {
+        if (empty($name) || empty($email) || empty($phone) || empty($address)) {
+            $errorMessage = 'All fields are required';
+            break;
+        }
+
+        $sql = "INSERT INTO clients (name,email,phone,address) VALUES ('$name','$email','$phone','$address')";
+        $result = $conn->query($sql);
+
+        if (!$result) {
+            $errorMessage = 'Invalid query: ' . $conn->error;
+            break;
+        }
+
+        $name = '';
+        $email = '';
+        $phone = '';
+        $address = '';
+        $successMessage = 'Client added correctly';
+
+        $conn->close();
+
+    } while (false);
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>My Shop</title>
+</head>
+<body>
+    <div class="container my-5">
+        <h2>New Client</h2>
+        <?php
+        if (!empty($errorMessage)) {
+            echo "
+            <div class='alert alert-warning alert-dismissible fade show' role='alert'>
+                <strong class='col-sm-3 form-label'>$errorMessage</strong>
+                <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='close'></button>
+            </div>
+            ";
+        }
+        ?>
+        <form method="post">
+            <div class="row mb-3">
+                <label class="col-sm-3 form-label">Name</label>
+                <div class="col-sm-6">
+                    <input type="text" class="form-control" name="name" value="<?php echo $name; ?>">
+                </div>
+            </div>
+            <div class="row mb-3">
+                <label class="col-sm-3 form-label">Email</label>
+                <div class="col-sm-6">
+                    <input type="email" class="form-control" name="email" value="<?php echo $email; ?>">
+                </div>
+            </div>
+            <div class="row mb-3">
+                <label class="col-sm-3 form-label">Phone</label>
+                <div class="col-sm-6">
+                    <input type="text" class="form-control" name="phone" value="<?php echo $phone; ?>">
+                </div>
+            </div>
+            <div class="row mb-3">
+                <label class="col-sm-3 form-label">Address</label>
+                <div class="col-sm-6">
+                    <input type="text" class="form-control" name="address" value="<?php echo $address; ?>">
+                </div>
+            </div>
+            <?php
+            if (!empty($successMessage)) {
+                echo "
+                <div class='alert alert-warning alert-dismissible fade show' role='alert'>
+                    <strong class='col-sm-3 form-label'>$successMessage</strong>
+                    <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='close'></button>
+                </div>
+                ";
+            }
+            ?>
+            <div class="row mb-3">
+                <div class="offset-sm-3 col-sm-3 d-grid">
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+                <div class="col-sm-3 d-grid">
+                    <a type="button" class="btn btn-outline-primary" href="/myshop/index.php">Cancel</a>
+                </div>
+            </div>
+        </form>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
--- db_connect.php
+++ db_connect.php
@@ -12,8 +12,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
 
 
-$conn->close();
+// $conn->close();
 ?>
