d2513d1b08f3d78a6224abec829702cad055c894
uwingabirejoselyne <uwingajoselyne@gmail.com>
2024-11-05 21:25:11 +0200

    added responsiviness for table clients

--- pages/tables.php
+++ pages/tables.php
@@ -7,22 +7,15 @@ include '../db_connect.php';
     <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
         <link rel="icon" type="image/png" href="../assets/img/favicon.png">
-        <title>
-            Dashboard by Creative joselyne
-        </title>
-        <!--     Fonts and icons     -->
+        <title>Dashboard by Creative joselyne</title>
         <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700,900" />
-        <!-- Nucleo Icons -->
         <link href="../assets/css/nucleo-icons.css" rel="stylesheet" />
         <link href="../assets/css/nucleo-svg.css" rel="stylesheet" />
-        <!-- Font Awesome Icons -->
         <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
-        <!-- Material Icons -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@24,400,0,0" />
-        <!-- CSS Files -->
-        <link id="pagestyle" href="../assets/css/material-dashboard.css?v=3.2.0" rel="stylesheet" />
+        <link href="../assets/css/material-dashboard.css?v=3.2.0" rel="stylesheet" />
+        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
     </head>
 
     <body class="g-sidenav-show  bg-gray-100">
@@ -49,59 +42,47 @@ include '../db_connect.php';
                             <span class="nav-link-text ms-1">Users</span>
                         </a>
                     </li>
-        <li class="nav-item">
-          <a class="nav-link text-dark" href="../pages/billing.html">
-            <i class="material-symbols-rounded opacity-5">receipt_long</i>
-            <span class="nav-link-text ms-1">Product</span>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link text-dark" href="../pages/virtual-reality.html">
-            <i class="material-symbols-rounded opacity-5">view_in_ar</i>
-            <span class="nav-link-text ms-1">Orders</span>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link text-dark" href="../pages/rtl.html">
-            <i class="material-symbols-rounded opacity-5">format_textdirection_r_to_l</i>
-            <span class="nav-link-text ms-1">Payment</span>
-          </a>
-        </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-dark" href="../pages/billing.html">
+                            <i class="material-symbols-rounded opacity-5">receipt_long</i>
+                            <span class="nav-link-text ms-1">Product</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-dark" href="../pages/virtual-reality.html">
+                            <i class="material-symbols-rounded opacity-5">view_in_ar</i>
+                            <span class="nav-link-text ms-1">Orders</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-dark" href="../pages/rtl.html">
+                            <i class="material-symbols-rounded opacity-5">format_textdirection_r_to_l</i>
+                            <span class="nav-link-text ms-1">Payment</span>
+                        </a>
+                    </li>
                 </ul>
             </div>
 
         </aside>
         <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
             <!-- Navbar -->
-            <nav class="navbar navbar-main navbar-expand-lg px-0 mx-3 shadow-none border-radius-xl" id="navbarBlur" data-scroll="true">
-                <div class="container-fluid py-1 px-3">
-                    <nav aria-label="breadcrumb">
-                        <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
-                            <li class="breadcrumb-item text-sm"><a class="opacity-5 text-dark" href="javascript:;">Pages</a></li>
-                            <li class="breadcrumb-item text-sm text-dark active" aria-current="page">Tables</li>
-                        </ol>
-                    </nav>
-                    <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4 d-flex justify-content-end" id="navbar">
-
-                        <ul class="navbar-nav d-flex align-items-center  justify-content-end">
-                            <li class="nav-item ">
-                                <a class="btn btn-outline-secondary btn-sm mb-0 me-3 " target="_blank" href="../pages/create.php">Add Client</a>
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <div class="container-fluid">
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse justify-content-end" id="navbar">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="btn btn-outline-secondary btn-sm mb-0 me-3" href="../pages/create.php">Add Client</a>
                             </li>
-                            <li>
-         <a class="dropdown-item" href="../pages/profile.html">
-            <i class="fas fa-user me-2"></i>
-        </a>
-    </li>
-    <li>
-        <a class="dropdown-item" href="./logout.php">
-            <i class="fas fa-sign-out-alt me-2"></i> Logout
-        </a>
-    </li>
-                            
 
+                            <li class="nav-item">
+                                <a class="nav-link" href="./logout.php"><i class="fas fa-sign-out-alt"></i> Logout</a>
+                            </li>
                         </ul>
-                        </div>
-                        </div>
+                    </div>
+                </div>
             </nav>
             <!-- End Navbar -->
             <div class="container-fluid py-2">
@@ -217,6 +198,8 @@ include '../db_connect.php';
         <script async defer src="https://buttons.github.io/buttons.js"></script>
         <!-- Control Center for Material Dashboard: parallax effects, scripts for the example pages etc -->
         <script src="../assets/js/material-dashboard.min.js?v=3.2.0"></script>
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
     </body>
 
     </html>
